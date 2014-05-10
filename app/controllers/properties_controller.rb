59d9cf93b06369f5376593e76d3095d11b7428e5
Muralee Mohan <muraleem3@gmail.com>
2014-05-10 18:44:23 +0530

    before swamy branch merge1

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,7 +2,9 @@ class PropertiesController < ApplicationController
 
 
 	def listproperty
-		@property = Property.new
+		
+        @property = Property.new
+		@property.features << Feature.new
 		@properties = Property.all
 	end
 
@@ -18,7 +20,31 @@ class PropertiesController < ApplicationController
 		@property = Property.new
 	end
 
-	def create
+	
+
+	def new
+		@property = Property.new
+		
 	end
+
+	def create
+		@property = Property.new(property_params)
+
+      respond_to do |format|
+        if @property.save
+          format.html { redirect_to @property, notice: 'Property was successfully Listed' }
+          #format.json { render action: 'show', status: :created, location: @exam }
+        else
+          format.html { render action: 'new' }
+          #format.json { render json: @exam.errors, status: :unprocessable_entity }
+        end
+      end
+	end
+
+	def property_params
+          params.require(:property).permit(:property_type,:property_type_code,:property_location,:property_locality,:property_min_price,:property_max_price,:property_area_measure,:property_image_path,:property_title,:property_description)
+    end
+
+
 
 end
